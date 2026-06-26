4dadf078169e988c35497c72fd62b27cd06ff9df
Monicah Ngugi <denismacharia151@gmail.com>
2026-06-26 20:45:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ try {
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
-} catch (\PDOException $e) { die("Database connection failed."); }
+} catch (\PDOException $e) { die("Database connection failed: " . $e->getMessage()); }
 
 // Handle Customer Upsell / Extra Part Change Requests
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit_request'])) {
